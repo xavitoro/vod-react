7f09678d29f9adb7f0c879be06677b2bd9863aa8
ASJ <aserrajaumot@gmail.com>
2016-12-09 12:32:51 +0100

    propTypes adapted to the recipes, style to be done

--- js/Recipe.js
+++ js/Recipe.js
@@ -1,28 +1,33 @@
 import React from 'react'
-const { string } = React.PropTypes
+const { string, number, shape, array } = React.PropTypes
 
 const ShowRecipe = React.createClass({
   propTypes: {
-    picturePlating: string.isRequired,
+    videoThumbnail: string.isRequired,
     title: string.isRequired,
     description: string.isRequired,
-    created: string.isRequired
-    // chefThumbnail: string.isRequired,
-    // created: number.isRequired,
-    // categories: array.isRequired,
+    created: number.isRequired,
+    author: shape({
+      name: string.isRequired,
+      thumbnail: string.isRequired
+    }),
+    categories: array.isRequired
   },
   render () {
+    const timestamp = Date(this.props.created)
+    // const date = (timestamp.getMonth()+1) + '/' + timestamp.getFullYear()
     return (
       <div className='show-card'>
-        <img src={`/public/img/recipes/${this.props.picturePlating}`} />
+        <img src={`/public/img/recipes/${this.props.videoThumbnail}`} />
         <div>
+          <h4>{this.props.author.name}</h4>
+          <img className='author-thumbnail' src={`/public/img/recipes/${this.props.author.thumbnail}`} />
           <h3>{this.props.title}</h3>
-          {/* <h4>{this.props.chef["name"]}</h4>
-          <h4>{this.props.chef["thumbnail"]}</h4> */}
-          <p>({this.props.created})</p>
-          {/* <p>({this.props.categories})</p> */}
+          <p>({timestamp})</p>
+          <p>({this.props.categories.toString()})</p>
           <p>{this.props.description}</p>
         </div>
+        <button>READ MORE</button>
       </div>
     )
   }
